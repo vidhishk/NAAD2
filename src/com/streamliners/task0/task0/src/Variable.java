465cb2df1cdb31724caf0b0b6711edb81017ebc9
vidhishk <76594187+vidhishk@users.noreply.github.com>
2021-04-02 17:10:27 +0530

    Delete src/com/streamliners/task0/task0/src directory

--- src/com/streamliners/task0/task0/src/Variable.java
+++
@@ -1,6 +0,0 @@
-public class Variable {
-    public static void main(String[] args) {
-        Double Number = -10.5 ;
-        System.out.print(Number);
-    }
-}
--- src/com/streamliners/task0/task0/src/addition.java
+++
@@ -1,9 +0,0 @@
-public class addition {
-    public static void main(String[] args) {
-        int p,q,r;
-        p=3;
-        q=4;
-        r=p+q;
-        System.out.print(r);
-    }
-}
