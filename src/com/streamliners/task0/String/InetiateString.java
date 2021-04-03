bd0203d48f8a56cab5a4197c3cd9991558ccfb58
vidhishk <76594187+vidhishk@users.noreply.github.com>
2021-04-03 23:23:17 +0530

    iterate each string characters.

---
+++ src/com/streamliners/task0/String/InetiateString.java
@@ -0,0 +1,10 @@
+public class InetiateString {
+    public static void main(String[] args) {
+        String name ="rajasthan";
+        System.out.println("the name is " +name);
+        for (int i =0;i<name.length();i++) {
+            char a = name.charAt(i);
+            {
+                System.out.print(a + ",");
+            }
+        }}}
