b93d136f8e05bf3922a698f2ec7787b742b04d99
vidhishk <76594187+vidhishk@users.noreply.github.com>
2021-04-03 19:30:02 +0530

    to find whether the number is positive or not.

---
+++ src/com/streamliners/task0/Decision making and loops/PositiveNegativeNum.java
@@ -0,0 +1,10 @@
+public class PositiveNegativeNum {
+    public static void main(String[] args){
+        int num= -5;
+        if(num>=0){
+            System.out.println("given number is positive");
+        }else {
+            System.out.println("number is negative");
+        }
+    }
+}
