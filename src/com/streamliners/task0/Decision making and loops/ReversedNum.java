67402001efb8df349480f96f661081fde458401e
vidhishk <76594187+vidhishk@users.noreply.github.com>
2021-04-03 19:24:37 +0530

    to find the reverse of the number.

---
+++ src/com/streamliners/task0/Decision making and loops/ReversedNum.java
@@ -0,0 +1,10 @@
+public class ReversedNum {
+    public static void main(String[] args) {
+        int num = 12345, reversed = 0;
+        for(;num != 0; num /= 10) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+        }
+        System.out.println("Reversed Number: " + reversed);
+        }
+}
