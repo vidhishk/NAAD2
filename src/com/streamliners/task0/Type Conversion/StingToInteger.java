191785147a0577d588e8beba69bcfaf9aae73007
vidhishk <76594187+vidhishk@users.noreply.github.com>
2021-04-03 13:25:32 +0530

    Add files via upload

---
+++ src/com/streamliners/task0/Type Conversion/StingToInteger.java
@@ -0,0 +1,7 @@
+public class StingToInteger {
+    public static void main(String[] args){
+        String str1 = "75";
+        int num1 = Integer.parseInt(str1);
+        System.out.println(num1);
+    }
+}
