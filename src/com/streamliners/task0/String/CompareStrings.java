c3489c4f8b850c114d1b8e842ebea18ab142b3ed
vidhishk <76594187+vidhishk@users.noreply.github.com>
2021-04-03 21:44:17 +0530

    compare two strings.

---
+++ src/com/streamliners/task0/String/CompareStrings.java
@@ -0,0 +1,11 @@
+public class CompareStrings {
+    public static void main(String[] args){
+        String style1="hello";
+        String style2="hello";
+        if (style1==style2){
+            System.out.println("equal");
+        }else{
+            System.out.println("not equal");
+    }}
+}
+
